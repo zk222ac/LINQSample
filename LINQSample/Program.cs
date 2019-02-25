832e9ef21cc5d35d31af2b87f04fbd3cd410cf46
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-02-25 13:45:21 +0100

    final version

--- LINQSample/Program.cs
+++ LINQSample/Program.cs
@@ -10,12 +10,46 @@ namespace LINQSample
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            // Data Source 
+            List<int> numbers = new List<int>(){0,1,4,8,2,6,5,7,3,9};
+
+            // Create Query 
+           var query = from n in numbers
+                where (n % 2 == 0)
+                orderby n
+                select n;
+
+            int count = query.Count();
+            Console.WriteLine($"Count: {count}");
+
+            var avg = query.Average();
+            Console.WriteLine($"Average : {avg}");
+
+            var max = query.Max();
+            Console.WriteLine($"Max : {max}");
+
+            var min = query.Min();
+            Console.WriteLine($"Min : {min}");
+
+            var first = query.First();
+            Console.WriteLine($"First : {first}");
+            PrintInfo(query);   
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
+
+        static void PrintInfo(dynamic query)
+        {
+            foreach (var num in query)
+            {
+                Console.WriteLine(num);
+            }
+        }
+       
+
+
     }
 }
